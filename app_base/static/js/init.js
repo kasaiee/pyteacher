c63a9cdb9c2d0e1f14fb622964ec447bd02a7d57
mrz <1tapciran@gmail.com>
2019-05-15 23:21:03 +0430

    add staticfiles

---
+++ app_base/static/js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('.tabs').tabs();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+$(document).ready(function() {
+    $('.card-content img').addClass('responsive-img');
+})
