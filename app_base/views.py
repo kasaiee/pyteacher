657c854355195b42dab6d74d703d14e71bfa12f3
mrz <1tapciran@gmail.com>
2019-06-04 18:42:15 +0430

    change to another way to serving protected media files

--- app_base/views.py
+++ app_base/views.py
@@ -12,23 +12,23 @@ from app_base.models import Course, CourseSession, ExerciseByStudent
 from django.contrib.auth.decorators import login_required
 from django.views.static import serve
 from django.conf import settings
-from sendfile import sendfile
+from django.http import HttpResponse
 
 
 def protected_serve(request, path, document_root=None, show_indexes=False):
     if 'session/private-videos' not in path:
-        # send myfile.pdf to user
-        return sendfile(request, settings.SENDFILE_ROOT + path)
+        return serve(request, path, document_root, show_indexes)
     else:
         item_id = path.split('/')[2]
         validate_cources = Course.objects.filter(id=item_id).first() in request.user.profile.registered_courses()
         validate_session = CourseSession.objects.filter(id=item_id).first() in request.user.profile.registered_sessions()
         if request.user.is_authenticated and (validate_session or validate_cources):
-            # send myfile.pdf to user
-            return sendfile(request, settings.SENDFILE_ROOT + path)
+            response = HttpResponse()
+            response["X-Accel-Redirect"] = f"/__media__/{path}"
+            del response["Content-Type"]
+            return response
         else:
-            # send myfile.pdf to user
-            return sendfile(request, settings.STATIC_ROOT + 'video/buy.mp4')
+            return serve(request, settings.STATIC_URL + 'video/buy.mp4', '', show_indexes)
 
 
 def home(request):
